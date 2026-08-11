c7bf18636786f58f3d0cfc2eb755552679a9f33a
10sinan <msinansonmez10@gmail.com>
2026-08-11 16:05:14 +0300

    MinIO Benchmark app ve proxy duzeltmeleri eklendi

--- benchmark_app/app.py
+++ benchmark_app/app.py
@@ -1,3 +1,12 @@
+import os
+# proxy engeleme için
+os.environ["NO_PROXY"] = "*"
+os.environ["no_proxy"] = "*"
+os.environ.pop("HTTP_PROXY", None)
+os.environ.pop("HTTPS_PROXY", None)
+os.environ.pop("http_proxy", None)
+os.environ.pop("https_proxy", None)
+
 import ui
 
 if __name__ == "__main__":
